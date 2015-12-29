de516a2edf32fc181d6c370c450801080e3f5656
Pavel Simonov <psimonov.web@gmail.com>
2015-12-29 12:35:06 +0300

    ...

---
+++ web/app/controllers/loginController.js
@@ -0,0 +1,6 @@
+loginController = function ($scope, $http) {
+    $http.get('http://rest-service.guides.spring.io/greeting').
+    success(function(data) {
+        $scope.greeting = data;
+    });
+};
