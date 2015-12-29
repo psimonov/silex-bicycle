5580a2e8a83c55888c435ed3e73ebb18b8a6de35
Pavel Simonov <psimonov.web@gmail.com>
2015-12-29 18:21:17 +0300

    ...

--- web/app/controllers/loginController.js
+++ web/app/controllers/loginController.js
@@ -1,8 +1,17 @@
+'use strict';
+
 //noinspection JSUnresolvedFunction
 var mainApp = angular.module('mainApp', []);
 
 mainApp.controller('loginController', function($scope, $http) {
-    $http.get('/api/list/').
+    console.log($scope.password);
+
+    $scope.password = '123';
+
+    $http.post('/admin/check/', {
+        '__username': $scope.username,
+        '__password': $scope.password
+    }).
     success(function(data) {
         $scope.greeting = data;
     });
